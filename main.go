8116112ddc18758a86d8381a931aab8df919e72f
fyambos <feliciteyambos@gmail.com>
2025-09-22 21:00:22 +0200

    selectionner un choix

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("\n--- Mini CRM ---")
-	fmt.Println("1. Ajouter un contact")
-	fmt.Println("2. Lister les contacts")
-	fmt.Println("3. Supprimer un contact")
-	fmt.Println("4. Quitter")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("\n--- Mini CRM ---")
+		fmt.Println("1. Ajouter un contact")
+		fmt.Println("2. Lister les contacts")
+		fmt.Println("3. Supprimer un contact")
+		fmt.Println("4. Quitter")
+
+		fmt.Print("Votre choix : ")
+		input, _ := reader.ReadString('\n')
+		choix, err := strconv.Atoi(strings.TrimSpace(input))
+
+		if err != nil {
+			fmt.Println("Erreur : veuillez entrer un nombre.")
+			continue
+		}
+
+		fmt.Println("Vous avez choisi :", choix)
+	}
 }
