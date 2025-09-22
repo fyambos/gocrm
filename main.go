bfa993715fa365d4ee27eb2be71041c9a6a104e5
fyambos <feliciteyambos@gmail.com>
2025-09-22 22:44:01 +0200

    feat: input ajout contact et affichage

--- main.go
+++ main.go
@@ -38,20 +38,31 @@ func main() {
 
 		switch choix {
 		case 1:
-			fmt.Println("Ajouter un contact")
-			contacts[nextID] = Contact{ID: nextID, Nom: "Alice", Email: "alice@mail.com"}
+			fmt.Print("Nom du contact : ")
+			nom, _ := reader.ReadString('\n')
+			nom = strings.TrimSpace(nom)
+
+			fmt.Print("Email du contact : ")
+			email, _ := reader.ReadString('\n')
+			email = strings.TrimSpace(email)
+
+			contacts[nextID] = Contact{ID: nextID, Nom: nom, Email: email}
 			fmt.Println("Contact ajouté avec ID", nextID)
 			nextID++
+
 		case 2:
-			fmt.Println("Lister les contacts")
+			fmt.Println("Liste des contacts :")
 			for _, c := range contacts {
 				fmt.Println(c.ID, c.Nom, c.Email)
 			}
+
 		case 3:
 			fmt.Println("Supprimer un contact")
+
 		case 4:
 			fmt.Println("Quitter")
 			return
+
 		default:
 			fmt.Println("Choix invalide.")
 		}
